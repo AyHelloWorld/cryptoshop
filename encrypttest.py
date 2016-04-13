8ef1fd3276862b140f4e2344f8e913ab1e24797b
antidote <antidote1911@gmail.com>
2016-04-13 13:20:30 +0200

    remove unnecessary argon2 folder accidentally pushed

--- encrypttest.py
+++ encrypttest.py
@@ -4,8 +4,8 @@
 from py_cryptoshop import encryptfile
 from py_cryptoshop import decryptfile
 
-#result = encryptfile(filename="test", passphrase="mypassword", algo="srp")
-#print(result["success"])
+result = encryptfile(filename="test", passphrase="mypassword", algo="srp")
+print(result["success"])
 
 result2 = decryptfile(filename="test.cryptoshop", passphrase="mypassword")
 print(result2["success"])
