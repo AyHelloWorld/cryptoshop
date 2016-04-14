e90fccc7652d1caff6940c7e82474b99207fef22
antidote <antidote1911@gmail.com>
2016-04-14 18:06:33 +0200

    typo

--- cryptoshop/__init__.py
+++ cryptoshop/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-    Cryptoshop version 1.0
+    Cryptoshop version 1.1
     Encrypt and decrypt file in CTR mode with AES, Serpent or Twofish as secure as possible.
     It use only strong algorithms, like Argon2 password derivation, or HMAC(Keccak-1600) for
     authentication.
