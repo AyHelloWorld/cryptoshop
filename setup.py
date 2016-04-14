ea712c101167c326ecf8581d084c21b921fcb43e
antidote <antidote1911@gmail.com>
2016-04-14 16:07:53 +0200

    setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Cryptoshop',
+    name='cryptoshop',
     version='1.0',
     packages=find_packages(),
     author="Fabrice Corraire",
@@ -26,5 +26,4 @@ setup(
         'Topic :: Software Development'
     ],
 
-    license="GPL",
 )
